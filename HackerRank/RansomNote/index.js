8294ebf863c066894f7d3e294bd074fe3b01a706
Matheus Almeida <mat.almeida@live.com>
2018-09-11 18:26:00 -0300

    HR: Solved Hash Tables: Ransom Note

---
+++ HackerRank/RansomNote/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+    let ret = 'No';
+    
+    magazine.map((magWord, index) => {
+        const idx = note.indexOf(magWord);
+        if( (idx != -1) && (magWord === note[idx]) ) {
+            note[idx] = idx;
+            magazine[index] = index + note.length;
+        }
+    })
+
+    if ( note.filter(item => typeof item != 'number').length == 0 ) ret = 'Yes';
+    
+    console.log(ret);
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
+
